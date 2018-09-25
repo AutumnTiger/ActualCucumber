24960f3f0ab0a698f562ddee50ec7247f21ea1da
Anubinda <anubinda@anubinda530s.desktops.cs.ait.ac.th>
2018-09-26 05:23:35 +0700

    main

---
+++ db/migrate/20180914020718_create_students.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :studentid
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
